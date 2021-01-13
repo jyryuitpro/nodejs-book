71e459d7a65b77de1d5fffc1edc1c90b689e86c4
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-13 20:50:36 +0900

    global과 콘솔, 타이머

--- lecture/func.js
+++ lecture/func.js
@@ -1,7 +1,7 @@
 // const value = require('./var');
 const {odd, even} = require('./var');
 // import {odd, even} from './var';
-
+console.time('jy.ryu.itpro');
 function checkOddOrEven(number) {
     if (number % 2) {
         // 홀수입니다
@@ -11,7 +11,7 @@ function checkOddOrEven(number) {
         return even;
     }
 }
-
+console.timeEnd('jy.ryu.itpro');
 module.exports = checkOddOrEven;
 // module.exports = {
 //     checkOddOrEven,
