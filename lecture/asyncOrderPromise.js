52cb2d5ae9b0f46dc218b9dec847f203d1de4571
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-17 15:35:07 +0900

    파일 시스템 사용하기

---
+++ lecture/asyncOrderPromise.js
@@ -0,0 +1,35 @@
+const fs = require('fs').promises;
+
+// fs.readFile('./readme.txt')
+//     .then((data) => {
+//         console.log('1번', data.toString());
+//         return fs.readFile('./readme.txt');
+//     })
+//     .then((data) => {
+//         console.log('2번', data.toString());
+//         return fs.readFile('./readme.txt');
+//     })
+//     .then((data) => {
+//         console.log('3번', data.toString());
+//         return fs.readFile('./readme.txt');
+//     })
+//     .then((data) => {
+//         console.log('4번', data.toString());
+//         return fs.readFile('./readme.txt');
+//     })
+//     .catch((err) => {
+//         throw err;
+//     });
+
+async function main() {
+    let data = await fs.readFile('./readme.txt');
+    console.log('1번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('2번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('3번', data.toString());
+    data = await fs.readFile('./readme.txt');
+    console.log('4번', data.toString());
+}
+
+main();
---
+++ lecture/writeFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+fs.writeFile('./writeme.txt', '글이 입력됩니다.')
+    .then(() => {
+        return fs.readFile('./writeme.txt');
+    })
+    .then((data) => {
+        console.log(data);
+        console.log(data.toString());
+    })
+    .catch((err) => {
+        throw err;
+    });
