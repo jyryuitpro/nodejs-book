5b41f544ed842f1591d2de8500f9452aa913a375
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-11 23:39:41 +0900

    이벤트루프 알아보기

---
+++ 02.알아두어야 할 자바스크립트/02.이벤트루프 알아보기.js
@@ -0,0 +1,24 @@
+// 이벤트루프
+// 메모리, 호출 스택 > 백그라운드 > 태스크 큐
+
+function oneMore() {
+    console.log('one more');
+}
+
+function run() {
+    console.log('run run');
+    setTimeout(() => {
+        console.log('wow');
+    }, 0)
+    new Promise((resolve => { // Promise의 내부까지는 동기
+        resolve('hi');
+    }))
+        .then(console.log); // Promise는 then을 만나는 순간 비동기이며, 백그라운드로 갈 때, resolve의 데이터 hi를 같이 준다. 즉, console.log('hi'); | 태스크 큐에서 우선순위가 높다
+    oneMore();
+}
+
+setTimeout(run, 500);
+
+// 태스크 큐에서 새치기를 한다. 예) setTimeout VS Promise의 then/catch, process의 nextTick
+// Promise의 then/catch
+// process의 nextTick
