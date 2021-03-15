1438c99622b03daa733ae03ab393b1d74697e7c4
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-15 23:00:20 +0900

    var, const, let

--- 02.알아두어야 할 자바스크립트/02.이벤트루프 알아보기.js
+++ 02.알아두어야 할 자바스크립트/02.이벤트루프 알아보기.js
@@ -10,15 +10,15 @@ function run() {
     setTimeout(() => {
         console.log('wow');
     }, 0)
-    new Promise((resolve => { // Promise의 내부까지는 동기
+    new Promise((resolve) => { // Promise의 내부까지는 동기
         resolve('hi');
-    }))
+    })
         .then(console.log); // Promise는 then을 만나는 순간 비동기이며, 백그라운드로 갈 때, resolve의 데이터 hi를 같이 준다. 즉, console.log('hi'); | 태스크 큐에서 우선순위가 높다
     oneMore();
 }
 
 setTimeout(run, 500);
 
-// 태스크 큐에서 새치기를 한다. 예) setTimeout VS Promise의 then/catch, process의 nextTick
+// 태스크 큐에서 새치기를 한다. 예) setTimeout VS (Promise의 then/catch, process의 nextTick)
 // Promise의 then/catch
 // process의 nextTick
